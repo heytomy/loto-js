042774952e17815708cf09e0332b298fe157e8ed
nisitomy <nisi.thomas@outlook.fr>
2022-12-22 10:59:21 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Grille de Loto</title>
+</head>
+<body>
+  <!-- Bouton pour générer une nouvelle grille -->
+  <button id="generer-grille-button">Générer une nouvelle grille</button>
+
+  <!-- Conteneur pour afficher la grille -->
+  <div id="grille-container"></div>
+
+  <script>
+    // Fonction pour générer une grille de loto
+    function genererGrilleLoto() {
+      // Tableau des nombres possibles pour une grille de loto
+      var nombres = [];
+      for (var i = 1; i <= 49; i++) {
+        nombres.push(i);
+      }
+
+      // Mélange aléatoirement les éléments du tableau
+      nombres.sort(() => Math.random() - 0.5);
+
+      // Prend les premiers 5 nombres du tableau mélangé comme nombres de la grille
+      var grille = nombres.slice(0, 5);
+
+      // Tri les nombres de la grille dans l'ordre croissant
+      grille.sort((a, b) => a - b);
+
+      // Retourne la grille générée
+      return grille;
+    }
+
+    // Référence au bouton "Générer une nouvelle grille"
+    var genererGrilleButton = document.getElementById('generer-grille-button');
+
+    // Ajoute un gestionnaire d'évènement pour générer une nouvelle grille lorsque le bouton est cliqué
+    genererGrilleButton.addEventListener('click', function() {
+      // Génère une grille de loto
+      var grille = genererGrilleLoto();
+
+      // Affiche la grille dans le conteneur
+      var grilleContainer = document.getElementById('grille-container');
+      grilleContainer.innerHTML = "Voici votre grille de loto : " + grille.join(", ");
+    });
+  </script>
+</body>
+</html>
